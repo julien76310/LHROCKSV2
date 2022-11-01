318d7178c9f35d1275a29d96d8cd4b5baf7f2f20
julien76310 <59526767+julien76310@users.noreply.github.com>
2022-11-01 09:16:08 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,93 +1,54 @@
-(function($, ElementQueries) {
-    document.addEventListener("arlojscontrolsloaded", function() {
-        var platformID = "websitetestdata.arlo.co"; // Change platformID to point at your own account
+var tgt, tgtPos;
+$('.overlay').on('click', function() {
+  //$(this).removeClass('current');
+  TweenMax.to($('.swiper-scrollbar'), 0, {autoAlpha:0});
+  TweenMax.to($('.swiper-scrollbar'), 1, {autoAlpha:1, delay:0.6});
+  $('.home').addClass('current');
+  TweenMax.to($('.overlay'),0.2, {x:tgtPos.left-20, y:tgtPos.top, width:tgt.outerWidth()+40, height:tgt.outerHeight(), autoAlpha:0});
+});
+$('.list-item').on('click', function() {
+  TweenMax.to($('.swiper-scrollbar'), 0, {autoAlpha:0});
+  TweenMax.to($('.swiper-scrollbar'), 1, {autoAlpha:1, delay:0.6});
+  $('.list-item').removeClass('current');
+  $(this).addClass('current');
+  $('.overlay').addClass('current');
+  tgt = $(this),
+  tgtPos = tgt.offset();
+  TweenMax.to($('.overlay'),0, {x:tgtPos.left-20, y:tgtPos.top, width:tgt.outerWidth()+40, height:tgt.outerHeight(), autoAlpha:1});
+  //swiperOverlay.update(true);
+  swiperOverlay.slideTo(0, 0, false);
+  $('.overlay .top').html(tgt.clone());
+  TweenMax.to($('.overlay'),0.2, {x:0, y:$(document).scrollTop(), width:'100%', height:'100%'});
+});
 
-        var filter = {
-            moduleType: "Filters",
-            targetElement: "#filters",
-            template: "#filter-template",
-            filterControlId: 1
-        };
+var swiperOverlay = new Swiper(".swiper-container.overlay", {
+  scrollbar: ".overlay > .swiper-scrollbar",
+  scrollbarHide: false,
+  scrollbarDraggable: true,
+  direction: "vertical",
+  slidesPerView: "auto",
+  mousewheelControl: true,
+  freeMode: true
+});
 
-        var eventList = {
-            moduleType: "UpcomingEvents",
-            targetElement: "#upcoming-events",
-            template: "#upcoming-events-template",
-            maxCount: 12,
-            filterControlId: 1,
-            includeLoadMoreButton: true,
-            loadMoreButtonText: "Show More",
-            includeArloLink: false,
-            smartDateFormats: {
-                startDay: "DD"
-            },
-            customUrls: {
-                eventtemplate: "/workshop/",
-                venue: "/venue/",
-                presenter: "/presenter/"
-            },
-            callbacks: {
-                onShow: eventListOnShowCallback
-            }
-        };
-
-        var app = new window.ArloWebControls();
-
-        app.start({
-            platformID: platformID,
-            showDevErrors: false,
-            modules: [eventList, filter]
-        });
-
-        /* ----- Callback function ----- */
-
-        // "OnShow" callback
-        function eventListOnShowCallback(getEventListItemsFunction) {
-            var listItems = getEventListItemsFunction();
-
-            var cardSummary = $('.arlo-summary');
-            var strMaxLength = 350;
-            
-            $.each(cardSummary, function (index, ele) {
-                var str = $(ele).text()
-                $(ele).text(strChopper(str));
-            });
-
-            function strChopper(str) {
-                if (str.length > strMaxLength) {
-                    str = str.substring(0, strMaxLength) + '...';
-                }
-                return str;
-            }
-            
-            ElementQueries.init();
-
-            // Hide timezone selector if there are no online events
-            if ($(".arlo-online").length < 1) {
-                $(".arlo-timezone-select").hide();
-            } else {
-                $(".arlo-timezone-select").show();
-                $(".arlo-timezone-select").parent().css("float", "right");
-            }
-
-            // Set up tooltips
-            $.each(listItems, function(index, listItem) {
-                var tooltipElement = $(listItem).find('[data-toggle="tooltip"]')[0];
-                if (tooltipElement) {
-                    var toolTipContent = $(listItem).find(".tooltipcontent")[0];
-                    if (toolTipContent) {
-                        $(tooltipElement).attr(
-                            "data-original-title",
-                            $(toolTipContent).html()
-                        );
-                        $(tooltipElement).tooltip();
-                    }
-                }
-            });
-        }
-
-        $("#arlo-filter-toggle").click(function() {
-            $(this).parent().toggleClass("arlo-show-filter");
-        });
-    });
-})(jQuery, window.ElementQueries);
+var swiperHome = new Swiper(".swiper-container.home", {
+  scrollbar: ".home .swiper-scrollbar",
+  scrollbarHide: false,
+  scrollbarDraggable: true,
+  direction: "vertical",
+  slidesPerView: "auto",
+  mousewheelControl: true,
+  freeMode: true,
+  onSliderMove: function(swiper, event) {
+    //console.log('moved');
+    moved = true;
+  },
+  onTouchEnd: function(swiper, event) {
+    //console.log('touchEnd');
+    TweenMax.delayedCall(0.1,resetmoved);
+    //moved = false;
+  }
+});
+function resetmoved() {
+  moved = false;
+}
